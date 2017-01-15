415947b1303ca63f832ea6e03787a8a368e20bfc
WeyensLuk <luk.weyens@gmail.com>
2017-01-15 19:28:46 +0100

    Add celluloid for actor-based async handling and resolve several ports asynchronously

--- portsniffer.rb
+++ portsniffer.rb
@@ -1,21 +1,53 @@
 require 'socket'
+require 'celluloid/autostart'
 
-puts 'Please provide an IP to probe'
+class PortScanner
+  include Celluloid
+
+  def initialize(ip, start_port, end_port)
+    @ip = ip
+    @start_port = start_port
+    @end_port = end_port
+  end
+
+  def scan
+    open_ports = []
+    for port_number in @start_port..@end_port
+      open_ports << is_port_open(port_number)
+    end
+    return open_ports.compact
+  end
+
+  def is_port_open(port_number)
+    begin
+      socket = TCPSocket.new(@ip, port_number)
+      return port_number
+    rescue Errno::ECONNREFUSED, Errno::EACCES, IO::EINPROGRESSWaitWritable
+    ensure
+      socket.close if socket
+    end
+  end
+end
+
+puts 'Please provide an IP to probe [127.0.0.1]'
 ip = gets.chomp
 ip = ip.to_s == '' ? '127.0.0.1' : ip
 
-def is_port_open(ip, port_number)
-  begin
-    socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
-    raw_port = Socket.sockaddr_in(port_number, ip)
-    return socket.connect(raw_port)
-  rescue Errno::ECONNREFUSED, Errno::EACCES
-    return false
-  end
+range = 100
+port_number = 1
+results = []
+
+until port_number >= 20000
+  port_scanner = PortScanner.new(ip, port_number, port_number + range)
+  (results << port_scanner.future.scan).flatten!
+
+  port_number += range + 1
+end
+results.map! {|port_number| port_number.value}
+results.flatten!
+
+for port_number in results.select {|port_number| port_number != nil}
+  puts "#{port_number} Open"
 end
 
-for port_number in 1..49151
-  if is_port_open(ip, port_number)
-    puts "#{port_number} Open"
-  end
-end
+puts 'Done generating!'
